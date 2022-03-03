a427c46b49a3ef538a24fb653bf1978c9c32c822
vanyagulati18 <vanyagulati.2008@gmail.com>
2022-03-03 20:22:57 +0530

    first commit

--- sketch.js
+++ sketch.js
@@ -1,40 +1,47 @@
 
- var backgroundImg,bg;
- var knightImg, knight;
- var enemiesImg, enemy;
+var backgroundImg, bg;
+var knightImg, knight;
+var enemiesImg, enemy;
+var invisibleGround;
 function preload() {
-  
-  
- backgroundImg = loadImage("background.jpg")
- knightImg = loadAnimation("assets/image1.png","assets/image2.png","assets/image3.png","assets/image4.png","assets/image5.png","assets/image6.png","assets/image7.png","assets/image8.png")
- enemiesImg = loadImage("enemies.png")
+
+
+  backgroundImg = loadImage("background.jpg")
+  knightImg = loadAnimation("assets/image1.png", "assets/image2.png", "assets/image3.png", "assets/image4.png", "assets/image5.png", "assets/image6.png", "assets/image7.png", "assets/image8.png")
+  enemiesImg = loadImage("enemies.png")
 }
 
-function setup(){
-  createCanvas(800,600);
-  bg = createSprite(width/2,height/2,width,height)
+function setup() {
+  createCanvas(800, 600);
+  bg = createSprite(width / 2, height / 2, width, height)
   bg.addImage(backgroundImg)
-  
   bg.velocityX = -5;
-  bg.x = width/2;
+  bg.x = width / 2;
 
 
-  knight = createSprite(width/2,height/2,40,40)
-  knight.addAnimation("running",knightImg)
+  invisibleGround = createSprite(400, 600, 800, 100)
+  invisibleGround.visible = false;
+
+
+
+  knight = createSprite(width / 2, height / 2, 40, 40)
+  knight.addAnimation("running", knightImg)
   knight.scale = 0.3;
 
 }
 function draw() {
   background("black");
- if(bg.x<-5){
-   bg.x = width/2;
+  if (bg.x < 300) {
+    bg.x = width / 2;
   }
-  if(keyDown("space")){
-    knight.velocityY =-10
+  if (keyDown("space")) {
+    knight.velocityY = -10
   }
-  knight.velocityY = knight.velocityY+0.8
-  
-  
+  knight.velocityY = knight.velocityY + 0.8
+
+  knight.collide(invisibleGround)
+
+
   drawSprites()
 }
 
