2802f4efc03caf165f955b3080fb8abe29be7869
vanyagulati18 <vanyagulati.2008@gmail.com>
2022-03-03 20:00:55 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+ var backgroundImg,bg;
+ var knightImg, knight;
+ var enemiesImg, enemy;
+function preload() {
+  
+  
+ backgroundImg = loadImage("background.jpg")
+ knightImg = loadAnimation("assets/image1.png","assets/image2.png","assets/image3.png","assets/image4.png","assets/image5.png","assets/image6.png","assets/image7.png","assets/image8.png")
+ enemiesImg = loadImage("enemies.png")
+}
+
+function setup(){
+  createCanvas(800,600);
+  bg = createSprite(width/2,height/2,width,height)
+  bg.addImage(backgroundImg)
+  
+  bg.velocityX = -5;
+  bg.x = width/2;
+
+
+  knight = createSprite(width/2,height/2,40,40)
+  knight.addAnimation("running",knightImg)
+  knight.scale = 0.3;
+
+}
+function draw() {
+  background("black");
+ if(bg.x<-5){
+   bg.x = width/2;
+  }
+  if(keyDown("space")){
+    knight.velocityY =-10
+  }
+  knight.velocityY = knight.velocityY+0.8
+  
+  
+  drawSprites()
+}
+
